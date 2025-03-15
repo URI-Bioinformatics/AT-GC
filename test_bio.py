bfcbd9db69e940db134bb4c29a01d6f24a4964ad
AymanBx <ayman_sandouk@uri.edu>
2025-03-15 01:53:18 -0400

    Excluding .gitkeep

--- test_bio.py
+++ test_bio.py
@@ -1,5 +1,7 @@
 import os
 from Bio import SeqIO
+# from Bio import LogisticRegression
+
 
 def read_fna(file_path):
     """Reads a .fna file and returns a list of SeqRecord objects."""
@@ -13,14 +15,16 @@ def read_fna(file_path):
 sequence_iterators = {}
 data_folder_path = "test_data"
 for file in os.listdir(data_folder_path):
-    file_path = os.path.join(data_folder_path, file)
-    if os.path.isfile(file_path):
-        sequence_iterators[file] = (read_fna(file_path))
+    if file != ".gitkeep":
+        file_path = os.path.join(data_folder_path, file)
+        if os.path.isfile(file_path):
+            sequence_iterators[file] = (read_fna(file_path))
 
 for file, sequences in sequence_iterators.items():
     print(f"Info from {file}")
-for record in sequences:
-    print(f"ID: {record.id}")
-    print(f"Sequence: {record.seq}")
-    print(f"Description: {record.description}")
-    print("---")
+
+    for record in sequences:
+        print(f"ID: {record.id}")
+        print(f"Sequence: {record.seq}")
+        print(f"Description: {record.description}")
+        print("---")
